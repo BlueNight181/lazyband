746e250491d773ee05790b609cc8b2b937c431e4
Nhat Ho <hominhnhat18@gmail.com>
2021-09-07 17:43:47 +0700

    asd

--- js/scrip.js
+++ js/scrip.js
@@ -23,4 +23,15 @@ window.addEventListener('scroll', function () {
   let header = document.getElementById('navbar');
   let windowPosition = window.scrollY > 600;
   header.classList.toggle('scrolling-active', windowPosition);
-})
+});
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 0) {
+    document.getElementById('ballon').style.visibility = 'unset';
+    $('#ballon').fadeIn();
+  } else {
+    document.getElementById('ballon').style.visibility = 'hidden';
+    // $('#ballon').fadeOut();
+  }
+});
