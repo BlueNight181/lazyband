e8043958890218306f441ce1a42f6c4d91bfc014
Nhat Ho <hominhnhat18@gmail.com>
2021-09-15 18:48:19 +0700

    mobile

--- js/scrip.js
+++ js/scrip.js
@@ -35,3 +35,5 @@ $(document).scroll(function() {
     // $('#ballon').fadeOut();
   }
 });
+
+
--- js/slick-slider.js
+++ js/slick-slider.js
@@ -2,7 +2,16 @@ $('.object__logo-content').slick({
  slidesToShow: 5,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 1000,
+  autoplaySpeed: 1200,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+  ]
 });
 // $('.carousel').carousel({
 //   interval: false,
