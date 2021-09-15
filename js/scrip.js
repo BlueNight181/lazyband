91f86f54045860a0e7e99f6f4035843dd9c50b84
Nhat Ho <hominhnhat18@gmail.com>
2021-09-15 19:24:56 +0700

    fix css

--- js/scrip.js
+++ js/scrip.js
@@ -2,28 +2,28 @@ function flyUp() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("scroll", myFunctionForSticky);
+// document.addEventListener("DOMContentLoaded", function () {
+//   window.addEventListener("scroll", myFunctionForSticky);
 
-  var navbar = document.getElementById("navbar");
-  var sticky = navbar.offsetTop;
+//   var navbar = document.getElementById("navbar");
+//   var sticky = navbar.offsetTop;
 
-  function myFunctionForSticky() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  }
-});
+//   function myFunctionForSticky() {
+//     if (window.pageYOffset >= sticky) {
+//       navbar.classList.add("sticky");
+//     } else {
+//       navbar.classList.remove("sticky");
+//     }
+//   }
+// });
 
 
 // menu changer background color
-window.addEventListener('scroll', function () {
-  let header = document.getElementById('navbar');
-  let windowPosition = window.scrollY > 600;
-  header.classList.toggle('scrolling-active', windowPosition);
-});
+// window.addEventListener('scroll', function () {
+//   let header = document.getElementById('navbar');
+//   let windowPosition = window.scrollY > 600;
+//   header.classList.toggle('scrolling-active', windowPosition);
+// });
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
